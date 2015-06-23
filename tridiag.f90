3342bc6e60db7f6d5e94f2b983327510d13766a2
Alex Kramer <alkramer@phys.ksu.edu>
2015-06-22 22:20:50 -0500

    Tridiagonal module abstraction

--- tridiag.f90
+++ tridiag.f90
@@ -10,7 +10,48 @@ module tridiag
   private
   public :: tridiag_sym_cnst
 
+  complex(dp), allocatable :: mat_coeff(:), vec_coeff(:)
+  integer(dp) :: n
+  
 contains
+
+  ! Initialize tridiagonal solver
+  ! Allocates work arrays for Thomas algorithm
+  subroutine tridiag_init(diag_arr)
+    implicit none
+
+    complex(dp) :: diag_arr(:)
+
+    n = size(diag_arr)
+    allocate(mat_coeff(n - 1))
+    allocate(vec_coeff(n))
+
+  end subroutine tridiag_init
+
+  ! Cleanup tridiagonal solver
+  subroutine tridiag_cleanup()
+    implicit none
+
+    deallocate(mat_coeff)
+    deallocate(vec_coeff)
+
+  end subroutine tridiag_cleanup
+
+  ! General backwards sweep for Thomas algorithm
+  subroutine tridiag_backsweep(res, vec_coeff)
+    implicit none
+
+    complex(dp), intent(inout) :: res(n)
+    complex(dp), intent(in) :: vec_coeff(n)
+    integer(dp) :: i
+    
+    res(n) = vec_coeff(n)
+
+    do i = n - 1, 1, -1
+       res(i) = vec_coeff(i) - mat_coeff(i) * res(i + 1)
+    end do
+    
+  end subroutine tridiag_backsweep
 
   ! Solves an n-dimensional tridiagional matrix equation A x = b, where the
   ! upper and lower diagional matrix elements are equal and constant.
@@ -18,15 +59,13 @@ contains
   ! n-dimensional tridiagonal system.
   subroutine tridiag_sym_cnst(diag, sym_cnst, vec, res)
     implicit none
-    ! todo: size checking - len(diag) = len(vec) = len(res)
+
     complex(dp), intent(in) :: diag(:), sym_cnst, vec(:)
     complex(dp), intent(out) :: res(:)
 
-    complex(dp), allocatable :: mat_coeff(:), vec_coeff(:)
-    integer(dp) :: n, i
+    integer(dp) :: i
 
-    n = size(diag)
-    allocate(mat_coeff(n), vec_coeff(n))
+    call tridiag_init(diag)
 
     ! Forward sweep
     mat_coeff(1) = sym_cnst / diag(1)
@@ -42,13 +81,8 @@ contains
          (diag(n) - sym_cnst * mat_coeff(n - 1))
 
     ! Backward sweep
-    res(n) = vec_coeff(n)
-
-    do i = n - 1, 1, -1
-       res(i) = vec_coeff(i) - mat_coeff(i) * res(i + 1)
-    end do
-
-    deallocate(mat_coeff, vec_coeff)
+    call tridiag_backsweep(res, vec_coeff)
+    call tridiag_cleanup()
     
-  end subroutine 
+  end subroutine
 end module tridiag
