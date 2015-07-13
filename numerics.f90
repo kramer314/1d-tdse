5b7a9faa79bc2519e0b376ae6c657c1327bda407
Alex Kramer <alkramer@phys.ksu.edu>
2015-07-13 11:37:07 -0500

    Add numerical second derivative and documentation edits

--- numerics.f90
+++ numerics.f90
@@ -12,6 +12,7 @@ module numerics
 
   public :: numerics_linspace
   public :: numerics_d1
+  public :: numerics_d2
 
 contains
 
@@ -39,7 +40,9 @@ contains
   end subroutine numerics_linspace
 
   ! Calculate the 1st derivative of a complex array-valued function with
-  ! respect to a real-valued coordinate.
+  ! respect to a real-valued coordinate with constant spacing:
+  !
+  !   f'(x) ~= (f(x + dx) - f(x - dx)) / (2 dx)
   !
   ! arr :: function array
   ! d_arr :: array to populate
@@ -55,14 +58,60 @@ contains
     n_x = size(arr)
     scale = 1.0_dp / (2.0_dp * dx)
 
-    d_arr(1) = (arr(2) - arr(1))
+    ! Special edge cases - we assume the grid is fine enough and the function
+    ! is smooth enough that this is accurate enough. For the left case:
+    !
+    !   f(x + h) = arr(2), f(x - h) = arr(1)
+    !
+    ! For the right case:
+    !
+    !   f(x + h) = arr(n), f(x - h) = arr(n - 1)
+    d_arr(1) = arr(2) - arr(1)
     do i_x = 2, n_x - 1
-       d_arr(i_x) = (arr(i_x + 1) - arr(i_x - 1))
+       d_arr(i_x) = arr(i_x + 1) - arr(i_x - 1)
     end do
-    d_arr(n_x) = (arr(n_x) - arr(n_x - 1))
+    d_arr(n_x) = arr(n_x) - arr(n_x - 1)
 
     d_arr(:) = scale * d_arr(:)
 
   end subroutine numerics_d1
+
+
+  ! Calculate the 2nd derivative of a complex array-valued function with
+  ! respect to a real-valued coordinate with constant spacing:
+  !
+  !   f''(x) ~= (f(x - dx) - 2 f(x) + f(x + dx)) / (dx**2)
+  !
+  ! arr :: function array
+  ! d2_arr :: array to populate
+  ! dx :: coordinate spacing
+  subroutine numerics_d2(arr, d2_arr, dx)
+
+    complex(dp), intent(in) :: arr(:)
+    complex(dp), intent(inout) :: d2_arr(:)
+    real(dp), intent(in) :: dx
+    integer(dp) :: i_x, n_x
+    real(dp) :: scale
+
+    n_x = size(arr)
+    scale = 1.0_dp / dx**2
+
+    ! Special edge cases - we assume the grid is fine enough and the function
+    ! is smooth enough that this is accurate enough. For the left case:
+    !
+    !   f(x + h) = arr(2), f(x - h) = arr(1)
+    !
+    ! For the right case:
+    !
+    !   f(x + h) = arr(n), f(x - h) = arr(n - 1)
+    d2_arr(1) = arr(1) + arr(2)
+    do i_x = 2, n_x - 1
+       d2_arr(i_x) = arr(i_x - 1) - 2 * arr(i_x) + arr(i_x + 1)
+    end do
+    d2_arr(n_x) = arr(n_x) + arr(n_x - 1)
+
+    d2_arr(:) = scale * d2_arr(:)
+
+  end subroutine numerics_d2
 
 end module numerics
